308a0eb3c9ec8f2cd66affc9b943b3901e52f303
vasundharag <vasundharag@vt.edu>
2024-04-14 00:50:03 +0000

    Update to Insurehub_Home.py

--- Insurehub_Home.py
+++ Insurehub_Home.py
@@ -19,6 +19,7 @@ col3, col1, col2 = st.columns([1,2, 1])
 logo1 = "./images/logo.png"
 image = Image.open(logo1)
 
+
 def get_image_as_base64(path):
     with open(path, "rb") as image_file:
         data = base64.b64encode(image_file.read()).decode()
@@ -92,5 +93,77 @@ if st.session_state.user_logged_in:
 
     helpers.sidebar.show()
 
-    st.markdown("Welcome to InsureHub!")
+    st.markdown("<h2>Welcome to InsureHub</h2>", unsafe_allow_html=True)
+    # Define the content of the section
+    section = "./images/insure.webp"
+    image = Image.open(section)
+    image_section = get_image_as_base64(section)
+    st.markdown(f"""
+    <div style="border: 2px solid #ccc; padding: 20px; display: flex; align-items: center;">
+        <div style="flex: 1;">
+            <h1 style="font-size: 36px; margin-bottom: 10px;">Fast way to get insured</h1>
+            <ul>
+                <li>Submit request online 24/7</li>
+                <li>Fast and intuitive process</li>
+                <li>Quality and real quotes</li>
+               <button onclick="location.href='/buy-renew';" style="margin:15px; cursor: pointer; background-color: transparent; border: 2px solid white; color: white; padding: 5px 10px;">Buy / Renew</button>
+            </ul>
+        </div>
+        <div style="flex: 1; text-align: center;">
+            <img src="{image_section}" alt="Insurance Image" style="max-width: 100%; height: auto;">
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
 
+
+    st.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>', unsafe_allow_html=True)
+
+
+    # Define function to extract video ID from URL
+    def extract_video_id(url):
+        return url.split("?v=")[-1]
+
+    # Display sections with YouTube videos side by side
+    st.markdown('<div style="display: flex;">', unsafe_allow_html=True)
+    st.markdown(f"""
+        <div  style="display :flex; justify-content:center;">
+            <div style="flex: 1; margin-right: 15px;">
+               <iframe width="700" height="400" src="https://www.youtube.com/embed/3ctoSEQsY54" title="How Does Insurance Work?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+        """, unsafe_allow_html=True)
+    st.markdown('</div>', unsafe_allow_html=True)
+
+    
+    # Insurance Icons
+    st.markdown("<h3>Our Insurance Services</h3>", unsafe_allow_html=True)
+    st.markdown("""
+        <div style="display:flex; justify-content: center;">
+            <div style="margin: 30px;"> 
+            <i class="fa-solid fa-car-burst fa-3x style="color: #66CC66;"></i>
+                <p>AutoSecure</p>
+            </div>
+            <div style="margin: 30px;">
+                <i class="fas fa-home fa-3x style="color: #66CC66;"></i>
+                <p>HomeSafe</p>
+            </div>
+            <div style="margin: 30px;">
+                <i class="fas fa-medkit fa-3x" style="color: #66CC66;"></i>
+                <p>Health</p>
+            </div>
+            <div style="margin: 30px;">
+                <i class="fa-solid fa-shield-dog fa-3x" style="color: #66CC66;"></i>
+                <p>PetCare</p>
+            </div>
+           
+            
+        </div>
+    """, unsafe_allow_html=True)
+
+        # Contact Us Section
+    st.markdown("<h3>Contact Us</h3>", unsafe_allow_html=True)
+    st.markdown("""
+        <p>Email: info@insurehub.com</p>
+        <p>Phone: 123-456-7890</p>
+        <!-- Add more contact details if necessary -->
+    """, unsafe_allow_html=True)
